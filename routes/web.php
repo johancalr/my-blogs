dfcf89850498205fa008df7248d1ab55420abe07
Johan Camilo Lozano Ramírez <johancalr11@gmail.com>
2024-09-25 20:50:35 -0500

    Create general routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/blog', function () {
+    // Consulta a la base de datos
+    $posts =  [
+        ['id' =>1, 'title' => 'PHP', 'slug' => 'php'],
+        ['id' =>2, 'title' => 'Laravel', 'slug' => 'laravel']
+    ];
+    return view('blog', ['posts' => $posts]);
+})->name('blog');
+
+Route::get('/blog/{slug}', function ($slug) {
+    // Consulta a la base de datos
+    $post = $slug;
+    return view('post', ['post' => $post]);
+})->name('post');
