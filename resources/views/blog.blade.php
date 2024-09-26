8ea1b234d929e1d4c2f3f1f58579e039b611be61
Johan Camilo Lozano Ramírez <johancalr11@gmail.com>
2024-09-25 20:51:43 -0500

    Add template for all routes

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,17 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-</head>
-<body>
+@extends('template')
+@section('content')
     <h1>Blog</h1>
     @foreach ($posts as $post)
     <p>
         <strong>{{$post['id']}}</strong>
-        <a href="">{{$post['title']}}</a>
+        <a href="{{ route('post', $post['slug']) }}">
+            {{$post['title']}}
+        </a>
     </p>
     @endforeach
-</body>
-</html>
+@endsection
